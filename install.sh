b8f61c7e90b261e99291733f3f205221ad0b43bd
Jean-Louis Jouannic <jean-louis.jouannic@zenika.com>
2019-09-20 15:13:06 +0200

    🚧 Continues wrinting install script
    - ZFS dataset creation
    - minimum system install
    - hostname configuration
    - network configuration for install
    - APT sources configuration

--- install.sh
+++ install.sh
@@ -5,7 +5,13 @@
 # instead of continuing the installation with something broken
 set -e
 
-# TARGET_DISK may be set as an environment variable
+# List of supported environment variables
+# TARGET_DISK path of the block device on which Ubuntu is to be installed (full disk).
+#   e.g: /dev/disk/by-id/nvme-KXG50ZNV512G_NVMe_TOSHIBA_512GB_Z74B602VKQJS
+# TARGET_HOSTNAME host name of the machine to be installed
+#   e.g: laptop-1
+# NETWORK_INTERFACE network interface to use for install
+#   e.g: wlp2s0
 
 check_root() {
     if [[ "${EUID}" -ne 0 ]]; then
@@ -34,9 +40,9 @@ select_disk() {
         done
 
         TARGET_DISK=$(whiptail --title "Disk to install to" --menu \
-        "Choose the device to install to (full disk)" 20 78 ${#disks[@]} \
-        "${options[@]}" \
-        3>&1 1>&2 2>&3)
+            "Choose the device to install to (full disk)" 20 78 ${#disks[@]} \
+            "${options[@]}" \
+            3>&1 1>&2 2>&3)
     fi
     echo "The chosen disk is: ${TARGET_DISK}"
 }
@@ -100,6 +106,72 @@ create_zfs_pools() {
       "${TARGET_DISK}-part4"
 }
 
+create_zfs_datasets() {
+    zfs create -o canmount=off -o mountpoint=none rpool/ROOT
+    zfs create -o canmount=off -o mountpoint=none bpool/BOOT
+
+    zfs create -o canmount=noauto -o mountpoint=/ rpool/ROOT/ubuntu
+    zfs mount rpool/ROOT/ubuntu
+
+    zfs create -o canmount=noauto -o mountpoint=/boot bpool/BOOT/ubuntu
+    zfs mount bpool/BOOT/ubuntu
+}
+
+install_minimum_system() {
+    debootstrap bionic /mnt
+    zfs set devices=off rpool
+}
+
+configure_hostname() {
+    if [ -z $TARGET_HOSTNAME ]; then
+        TARGET_HOSTNAME=$(whiptail --inputbox \
+            "How do you want to name this machine?" 8 78 "" \
+            --title "Host name" 3>&1 1>&2 2>&3)
+    fi
+
+    echo "${TARGET_HOSTNAME}" > /mnt/etc/hostname
+    echo "127.0.1.1       ${TARGET_HOSTNAME}" >> /mnt/etc/hosts
+}
+
+configure_network() {
+    local links
+    local options
+    if [ -z $NETWORK_INTERFACE ]; then
+        # TO IMPROVE
+        mapfile -t links < <( ip route show | grep "default via " | awk -F " " '{print $5}' )
+        if [ ${#links[@]} -eq 1 ]; then
+            NETWORK_INTERFACE=${links[0]}
+        elif [ ${#links[@]} -gt 1 ]; then
+            for i in "${!links[@]}"; do
+                options+=("${links[i]}")
+                options+=("")
+            done
+            NETWORK_INTERFACE=$(whiptail --title "Network interface" --menu \
+                "Choose the network interface to use for install" 20 78 ${#links[@]} \
+                "${options[@]}" \
+                3>&1 1>&2 2>&3)
+        else
+            echo "Unable to find a suitable network interface"
+            exit 1
+        fi
+    fi
+
+    mkdir -p /mnt/etc/netplan/
+    { \
+        echo "network:"; \
+        echo "  version: 2"; \
+        echo "  ethernets:"; \
+        echo "    ${NETWORK_INTERFACE}:"; \
+        echo "      dhcp4: true"; \
+    } >> /mnt/etc/netplan/01-netcfg.yaml
+}
+
+configure_apt_sources() {
+
+    echo "deb http://security.ubuntu.com/ubuntu bionic-security main universe" >> /mnt/etc/apt/sources.list
+    echo "deb http://archive.ubuntu.com/ubuntu bionic-updates main universe" >> /mnt/etc/apt/sources.list
+}
+
 main() {
     check_root
 
@@ -112,6 +184,16 @@ main() {
     create_partitions
 
     create_zfs_pools
+
+    create_zfs_datasets
+
+    install_minimum_system
+
+    configure_hostname
+
+    configure_network
+
+    configure_apt_sources
 }
 
 main
