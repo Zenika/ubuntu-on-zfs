b5e40d212d0b35e2ad4be518ce2b3767f903468f
Jean-Louis Jouannic <jean-louis.jouannic@zenika.com>
2019-10-10 15:09:20 +0200

    ✨ Sets keyboard configuration

--- chroot-install.sh
+++ chroot-install.sh
@@ -27,6 +27,37 @@ configure_tzdata() {
     dpkg-reconfigure --frontend=noninteractive tzdata
 }
 
+configure_keyboard() {
+    # Do NOT format code below. Tabs are IMPORTANT. Do not remove any! Do NOT add any!
+    cat <<EOT >> /tmp/deb-keyboard.conf
+keyboard-configuration	console-setup/ask_detect	boolean	false
+keyboard-configuration	keyboard-configuration/model	select	PC générique 105 touches (internat.)
+keyboard-configuration	keyboard-configuration/layoutcode	string	fr
+keyboard-configuration	keyboard-configuration/variant	select	Français
+keyboard-configuration	keyboard-configuration/unsupported_layout	boolean	true
+keyboard-configuration	keyboard-configuration/xkb-keymap	select
+keyboard-configuration	keyboard-configuration/ctrl_alt_bksp	boolean	false
+keyboard-configuration	keyboard-configuration/unsupported_options	boolean	true
+keyboard-configuration	keyboard-configuration/optionscode	string
+keyboard-configuration	keyboard-configuration/unsupported_config_layout	boolean	true
+keyboard-configuration	keyboard-configuration/modelcode	string	pc105
+keyboard-configuration	keyboard-configuration/store_defaults_in_debconf_db	boolean	true
+keyboard-configuration	keyboard-configuration/variantcode	string
+keyboard-configuration	console-setup/detected	note
+keyboard-configuration	keyboard-configuration/compose	select	No compose key
+keyboard-configuration	keyboard-configuration/switch	select	No temporary switch
+keyboard-configuration	keyboard-configuration/unsupported_config_options	boolean	true
+keyboard-configuration	keyboard-configuration/layout	select	Français
+keyboard-configuration	keyboard-configuration/altgr	select	The default for the keyboard layout
+keyboard-configuration	keyboard-configuration/toggle	select	No toggling
+EOT
+    debconf-set-selections < /tmp/deb-keyboard.conf
+    dpkg-reconfigure --frontend=noninteractive keyboard-configuration
+    rm /tmp/deb-keyboard.conf
+
+    sed -i -e 's/XKBLAYOUT="\w*"/XKBLAYOUT="fr"/' /etc/default/keyboard
+}
+
 install_zfs() {
     apt install --yes --no-install-recommends linux-image-generic
     apt install --yes zfs-initramfs
@@ -135,6 +166,8 @@ main() {
     configure_locales
 
     configure_tzdata
+
+    configure_keyboard
 
     install_zfs
 
--- first-boot.sh
+++ first-boot.sh
@@ -5,43 +5,16 @@
 # instead of continuing the installation with something broken
 set -e
 
-configure_keyboard() {
-    # Do NOT format code below. Tabs are IMPORTANT. Do not remove any! Do NOT add any!
-    cat <<EOT >> /tmp/deb-keyboard.conf
-keyboard-configuration	console-setup/ask_detect	boolean	false
-keyboard-configuration	keyboard-configuration/model	select	PC générique 105 touches (internat.)
-keyboard-configuration	keyboard-configuration/layoutcode	string	fr
-keyboard-configuration	keyboard-configuration/variant	select	Français
-keyboard-configuration	keyboard-configuration/unsupported_layout	boolean	true
-keyboard-configuration	keyboard-configuration/xkb-keymap	select
-keyboard-configuration	keyboard-configuration/ctrl_alt_bksp	boolean	false
-keyboard-configuration	keyboard-configuration/unsupported_options	boolean	true
-keyboard-configuration	keyboard-configuration/optionscode	string
-keyboard-configuration	keyboard-configuration/unsupported_config_layout	boolean	true
-keyboard-configuration	keyboard-configuration/modelcode	string	pc105
-keyboard-configuration	keyboard-configuration/store_defaults_in_debconf_db	boolean	true
-keyboard-configuration	keyboard-configuration/variantcode	string
-keyboard-configuration	console-setup/detected	note
-keyboard-configuration	keyboard-configuration/compose	select	No compose key
-keyboard-configuration	keyboard-configuration/switch	select	No temporary switch
-keyboard-configuration	keyboard-configuration/unsupported_config_options	boolean	true
-keyboard-configuration	keyboard-configuration/layout	select	Français
-keyboard-configuration	keyboard-configuration/altgr	select	The default for the keyboard layout
-keyboard-configuration	keyboard-configuration/toggle	select	No toggling
-EOT
-    debconf-set-selections < /tmp/deb-keyboard.conf
-    dpkg-reconfigure --frontend=noninteractive keyboard-configuration
-    rm /tmp/deb-keyboard.conf
-}
+
 
 clean_itself() {
     rm /usr/local/first-boot.sh
 }
 
 main() {
-    configure_keyboard
+    echo "Coucou !"
 
-    clean_itself
+    #clean_itself
 }
 
 main
